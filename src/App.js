e00e915e905d157eb2a7e1f7f080966711f57aca
yann <yanndubois4@gmail.com>
2020-04-15 14:42:03 +0200

    Fixing link issue in App.jsr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "./components/NavBar";
+import GpsIcon from "./components/icons/GpsIcon";
+import ListIcon from "./components/icons/ListIcon";
+import HeartIconClicked from "./components/icons/HeartIconClicked";
+import HeartIcon from "./components/icons/HeartIcon";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <GpsIcon />
+      <ListIcon />
+      <HeartIconClicked />
+      <HeartIcon />
     </div>
   );
 }
